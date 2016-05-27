5310ec1f14f2bcc9d7a107ba7359b2355110dbf1
gabe351 <gabrielrosadn@gmail.com>
2016-05-27 18:28:00 -0300

    mais um menu adicionado, para verificar a conta

--- main.rb
+++ main.rb
@@ -3,9 +3,10 @@ require_relative "caixa"
 def getvalue
   gets.to_i
 end
+
 #menu
 def menu(caixa)
-  puts "\nOla, qual operacao voce deseja?"
+  puts "qual operacao voce deseja?"
   puts "1- deposito"
   puts "2- saque"
   puts "3- saldo"
@@ -25,13 +26,33 @@ def menu(caixa)
     puts "voce selecionou saldo"
       caixa.balance
   elsif opc == 4
-    return
+    menu_conta
   else
     puts "Por favor, digite uma opcao valida \n"
   end
   menu(caixa) #recursividade do método
 end
 
-caixa = Caixa.new(0)
-#chamando menu
-menu(caixa)
+def menu_conta
+  caixa = Caixa.new #MELHORAR A CONTA, TENTAR TRABALHAR COM ARRAY/ Tirar duvida com chid
+
+  puts "Bem vindo, digite o numero da sua conta ou digite 0 para sair"
+  conta = getvalue
+  if conta == 24
+    puts "Bem vindo gijo!"
+    menu(caixa)
+  elsif conta == 10
+    puts "Bem vindo champs!"
+    menu(caixa)
+  elsif conta == 0
+    exit #quando coloquei return ele continuava chamando o menu(caixa) sozinho
+  else
+    puts "Nao tem conta cadastrada irmao"
+    puts ""
+    menu_conta
+  end
+end
+
+
+
+menu_conta
